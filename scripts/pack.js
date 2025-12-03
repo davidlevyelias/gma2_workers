88cc760c1fde0fb0d9e1ece371e4b25f5b435b3d
David Levy <86852268+ld-davidlevy@users.noreply.github.com>
2025-12-03 15:07:08 -0300

    refactor: Update workspace settings and rename output file for better compatibility (remove hiphen)

--- scripts/pack.js
+++ scripts/pack.js
@@ -2,7 +2,7 @@ const { spawnSync } = require('child_process');
 const path = require('path');
 const pkg = require('../package.json');
 
-const outputPath = path.join('dist', `gma2-workers-v${pkg.version}.lua`);
+const outputPath = path.join('dist', `gma2workers_v${pkg.version}.lua`);
 
 const result = spawnSync('npx', ['luapack', '--config', 'luapack.config.json', '--output', outputPath], {
     stdio: 'inherit',
